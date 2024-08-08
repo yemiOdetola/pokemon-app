2c8c3b12c1ba33993a2dfc63f0722b9d12562176
Odetola Azeez Opeyemi <yemiotola@gmail.com>
2024-08-08 14:09:23 +0100

    added docker start deployment

---
+++ docker-start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Wait for the database to be ready
+until nc -z -v -w30 db 5432
+do
+  echo "Waiting for database connection..."
+  sleep 1
+done
+
+# Run migrations
+npx prisma migrate deploy
+
+# Generate data
+npx ts-node prisma/generateData.ts
+
+# Import Pokemon
+npx ts-node prisma/importPokemon.ts
+
+# Start the application
+npm run start:prod
